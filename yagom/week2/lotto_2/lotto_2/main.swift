622f34262b1db4f8ca6fd04bd974a4e25e91bcef
j1nt0 <sk2018univ@naver.com>
2023-03-06 00:06:28 +0900

    2023-03-06

--- yagom/week2/lotto_2/lotto_2/main.swift
+++ yagom/week2/lotto_2/lotto_2/main.swift
@@ -5,45 +5,28 @@
 //  Created by 이진 on 2023/03/02.
 //
 
-func setIntToString(setInt: Set<Int>) -> String {
-    let setInt = setInt
-    let setString = setInt.map {String($0)}
-    let string = setString.joined(separator: ", ")
+func makeRandomArray(setLength: Int) -> Array<Int> {
+    var randomSet = Set<Int>()
     
-    return string
-}
-
-func makeRandomSet(SetLength: Int) -> Set<Int> {
-    var RandomSet = Set<Int>()
-    
-    while (RandomSet.count < SetLength) {
-        RandomSet.insert(Int.random(in: 1...45))
+    while (randomSet.count < setLength) {
+        randomSet.insert(Int.random(in: 1...45))
     }
     
-    return RandomSet
+    let randomArray = Array(randomSet)
+    
+    return randomArray
 }
 
-func checkLotto(myNumbers: Int...) {
-    let lottoNumbers = makeRandomSet(SetLength: 6)
-    let myNumbers = myNumbers
+func checkLottoNumber(round: Int) {
+    var lottoDictionary = Dictionary<String, Any>()
     
-    var correctNumbers = Set<Int>() //로또와 비교해 맞힌 숫자를 저장할 Set 선언
-    
-    for myNumber in myNumbers {
-        if lottoNumbers.contains(myNumber) {
-            correctNumbers.insert(myNumber)
-        }
+    for round in 1...5{
+        lottoDictionary["\(round)회차"] = makeRandomArray(setLength: 6)
     }
     
-    let correctNumberString = setIntToString(setInt: correctNumbers)
+    let selectedRoundLottoNumber: Any? = lottoDictionary["\(round)회차"]
     
-    if correctNumbers.count == 0 {
-        print("아쉽지만 겹치는 번호가 없습니다.")
-    } else {
-        print("축하합니다! 겹치는 번호는 ", terminator: "")
-        print(correctNumberString, terminator: "")
-        print(" 입니다!")
-    }
+    print(selectedRoundLottoNumber)
 }
 
-checkLotto(myNumbers: 1,2,3,4,5,6)
+checkLottoNumber(round: 4)
